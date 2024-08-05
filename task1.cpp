c22072839ff445e67551c514eddbf71e11339dbf
kartikeRathore <143009595+kartikeRathore@users.noreply.github.com>
2024-08-05 19:17:53 +0530

    Add files via upload
     number guessing game

---
+++ task1.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    // Seed the random number generator
+    srand(time(0));
+
+    // Generate a random number between 1 and 100
+    int secretNumber = rand() % 100 + 1;
+    int guess;
+
+    cout << "Guess the number (between 1 and 100): ";
+
+    while (true) {
+        cin >> guess;
+
+        if (guess == secretNumber) {
+            cout << "Congratulations! You guessed the number." << endl;
+            break;
+        } else if (guess < secretNumber) {
+            cout << "Too low! Try again." << endl;
+        } else {
+            cout << "Too high! Try again." << endl;
+        }
+    }
+
+    return 0;
+}
